4a90a09624fa2666bedbf3e8dc17a79b323e6da6
Deepraj <rajd7594@gmail.com>
2024-03-29 14:28:50 +0530

    website made

---
+++ src/app/constants/index.ts
@@ -0,0 +1,47 @@
+export const inclusions = [
+  {
+    title: 'Quality Shipping',
+    description: 'Quality shipping by trusted partners',
+    icon: '/assets/icons/shipping1.svg',
+  },
+  {
+    title: 'Money Guarantee',
+    description: 'Within 30 days for an exchange',
+    icon: '/assets/icons/dollar1.svg',
+  },
+  {
+    title: 'Online Support',
+    description: '24 hours a day, 7 days a week',
+    icon: '/assets/icons/support1.svg',
+  },
+  {
+    title: 'Flexible Payment',
+    description: 'Pay with multiple credit cards',
+    icon: '/assets/icons/payment1.svg',
+  },
+]
+
+export const profileNavItems = [
+  {
+    title: 'Personal Information',
+    url: '/account',
+    icon: '/assets/icons/user.svg',
+  },
+  {
+    title: 'My Purchases',
+    url: '/account/purchases',
+    icon: '/assets/icons/purchases.svg',
+  },
+  {
+    title: 'My Orders',
+    url: '/account/orders',
+    icon: '/assets/icons/orders.svg',
+  },
+  {
+    title: 'Logout',
+    url: '/logout',
+    icon: '/assets/icons/logout.svg',
+  },
+]
+
+export const noHeaderFooterUrls = ['/create-account', '/login', '/recover-password']
